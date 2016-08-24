d33b64044d5e45ee5e52e38913bdc244a47fd06a
Michael Q <MichaelQuiban@outlook.com>
2016-08-23 20:48:35 -0700

    Added Javascript
    Added Function that generates new content on the UI once "New game" has
    been selected.
    
    Added event listeners for the submit button to generate new text for
    different scenarios the user puts themselves in:
    Count Number
    Guess Counter
    Append these values.
    
    Also changed checkGuess to calculate the difference in a function and
    return different forms of hot and cold based on user input.

--- js/app.js
+++ js/app.js
@@ -12,17 +12,68 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
-     var newGame = function(e){
-    
- }
+    $(".new").click(function(){
+      newGame();
+    })
 
+    //Global variables for new game.
+    var randomNumber, counter;
+
+    function newGame(){
+      //Generate a random number from 0 - 100.
+      randomNumber = Math.floor(Math.random() * 100) + 1;
+      // We'll begin at 0..
+      counter = 0;
+      $("#feedback").text("Make your guess!");
+      $("#userGuess").val("");
+      //empty the list items
+      $("#guessList").empty();
+      $("#count").text(0);
+    }
+  
+  //event listener, when the user click the button, or hits return
+  // it will generate a submit event, when received, this will run!
+  $("#guess-form").submit(function(e){
+    e.preventDefault();
+    //get input value from id user-guess
+    var userGuess = $("#userGuess").val();
+    //compute the difference
+    var diff = Math.abs(randomNumber - userGuess);
+    counter++;
+    //returns the result
+    $("#feedback").text(checkGuess(diff));
+    $("#count").text(counter);
+    $("#guessList").append('<li>' + userGuess + '</li>');
+   })
+
+
+  //'Hot' or 'Cold' is based on a less than and greater than system.
+  function checkGuess(diff) {
+      if (diff > 80) {
+        return "Ice Cold";
+      }
+      // User will receive 'Hot' when the answer is greater.
+      if (diff > 60 ) {
+        return "Cold";
+      }
+       if (diff > 40 ) {
+        return "Eh kinda cold";
+      }
+       if (diff > 20 ) {
+        return "Oh man it's getting there";
+      }
+       if (diff > 10 ) {
+        return "Uhhh, you're close";
+      }
+       if (diff > 5 ) {
+        return "You're on fire!";
+      }
+       if (diff > 2 ) {
+        return "Burning Burning Call 911!";
+      }
+       if (diff > 0 ) {
+        return "You finally found me!";
+      }
+    }
+    newGame();
 });
-
-//Creates a random number for the user to guess.
-function randomNumber (upper, lower) {
-  lower = Math.ceil(lower);
-  upper = Math.floor(upper);
-  return Math.floor(Math.random() * (upper - lower + 1)) + lower;
-}
-
-console.log(randomNumber(2,5));
